2f54fcac5531ca8e2d7382df7899317b61aa5fee
aarayhan <array248@gmail.com>
2026-07-23 07:51:17 +0700

    fix bab5 name

---
+++ bab5/map.dart
@@ -0,0 +1,26 @@
+void main() {
+//Map: Menyimpan Data dengan Format Key-Value
+  // Membuat Map biodata
+  Map<String, String> biodata = {
+    'nama': 'Ahmad Zaki',
+    'kelas': 'XI RPL 1',
+    'alamat': 'Bandung'
+  };
+
+  // Mengakses nilai berdasarkan key
+  print('Nama siswa: ${biodata['nama']}'); // Output: Ahmad Zaki
+
+  // Menampilkan semua keys
+  print('Keys: ${biodata.keys}');
+
+  // Menampilkan semua values
+  print('Values: ${biodata.values}');
+
+  // Menambah data baru
+  biodata['hobi'] = 'Membaca Buku';
+  print (biodata);
+
+  // Mengubah data
+  biodata['alamat'] = 'Jakarta';
+  print (biodata);  
+}
---
+++ bab5/set.dart
@@ -0,0 +1,16 @@
+void main() {
+  //Set: Menyimpan Kumpulan Data Unik
+  // Membuat Set
+  Set<int> nomorUndian = {10, 25, 5, 10, 30, 5};
+
+  // Angka 10 dan 5 yang duplikat akan otomatis diabaikan
+  print(nomorUndian); // Output: {10, 25, 5, 30} (urutan bisa berbeda)
+
+  // Menambah elemen
+  nomorUndian.add(40);
+  print(nomorUndian);
+
+  // Menghapus elemen
+  nomorUndian.remove(5);
+  print(nomorUndian);
+}
